3dbfa686da613847dd374ff827ba0a84da2e2a1c
bot17171661 <53573943+bot17171661@users.noreply.github.com>
2020-09-11 18:49:38 +0300

    1

--- dev/blocks/radio.js
+++ dev/blocks/radio.js
@@ -9,8 +9,30 @@ Block.createBlock("iMod_radio", [
 	}
 ]);
 
+var maxRadio = 0;
+var radios = [new Sound("0.ogg")]
 TileEntity.registerPrototype(BlockID.iMod_radio, {
+    defaulValues:{
+        currentRadio: 0,
+        radioPlay: false
+    },
     init: function(){
         alert('Hi');
+        this.data.radioPlay = false;
+    },
+    click: function(){
+        if(Entity.getSneaking(Player.get())){
+            radios[this.data.currentRadio].stop();
+            return false;
+        }
+        setRadio(this.data.currentRadio + 1);
+        return true;
+    },
+    setRadio: function(_id){
+        if(_id >= maxRadio) _id = 0;
+        radios[this.data.currentRadio].stop();
+        this.data.currentRadio = _id;
+        radios[_id].play();
+        this.data.radioPlay = true;
     }
 })
